38ef0b5d46b102d8864efd0cd0b9d2f1fbaf351c
asha <asha29178@gmail.com>
2021-10-01 08:54:21 +0700

    set page dashbord

--- pages/backoffice/index.tsx
+++ pages/backoffice/index.tsx
@@ -1,8 +1,36 @@
+import { Card, Col, Row } from "antd";
 import React from "react";
 import BackLayout from "../../containers/BackLayout";
+import { useRecoilState } from "recoil";
+import { Product, Orders } from "../../lib/recoil-atom";
 
 const BackOffice = () => {
-  return <BackLayout>Back Office</BackLayout>;
+  const [productList] = useRecoilState(Product);
+  const [orderList] = useRecoilState(Orders);
+  return (
+    <BackLayout>
+      <Row gutter={[24, 24]} className="p-1">
+        <div className="px-1 w-100">
+          <h2 className="tw-b mb-0">Dashboard</h2>
+        </div>
+
+        <Col lg={12}>
+          <Card className="card-radius10">
+            <div className="d-flex items-ceter justify-between">
+              <h2>All Product</h2> <h2>|</h2> <h2>{productList.length}</h2>
+            </div>
+          </Card>
+        </Col>
+        <Col lg={12}>
+          <Card className="card-radius10">
+            <div className="d-flex items-ceter justify-between">
+              <h2>All Order</h2> <h2>|</h2> <h2>{orderList.length}</h2>
+            </div>
+          </Card>
+        </Col>
+      </Row>
+    </BackLayout>
+  );
 };
 
 export default BackOffice;
